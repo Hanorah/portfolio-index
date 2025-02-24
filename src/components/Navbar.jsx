367085939f434e0ebdfa01edd261f84e6209417a
Hanorah <ksdnorah@gmail.com>
2025-02-24 04:12:34 +0100

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,10 +76,10 @@ const NavBar = () => {
       className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
     >
       <header className="absolute top-1/2 w-full -translate-y-1/2">
-        <nav className="flex size-full items-center justify-between p-4">
+        <nav className="flex size-full items-center justify-between p-2">
           {/* Logo and Product button */}
-          <div className="flex items-center gap-7">
-            <img src="/img/logo.png" alt="logo" className="w-10" />
+          <div className="flex items-center gap-4">
+            <img src="/img/logo.png" alt="logo" className="w-8" />
 
             <a
               href="https://portfolio-fqgncfzah-hanorahs-projects.vercel.app/projects"
